4befd9e63e33a770ac1e3a5fdc5a3f59ae5567e8
Alon Greyber <alongreyber@gmail.com>
2020-03-02 23:35:07 -0500

    Deleted lots and lots of old code

--- frontend-v-old/src/utils/message.ts
+++
@@ -1,9 +0,0 @@
-export default class Message {
-    private text: string;
-    private color: string;
-    constructor(text: string, color: string) {
-	this.text = text;
-	this.color = color;
-    }
-}
-
